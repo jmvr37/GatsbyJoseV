638112a485d63798837053e19d47b59774416451
jose <jmvr37@hotmail.com>
2021-06-23 18:11:36 -0700

    change web icon for shiba doggy

--- src/pages/Generic.js
+++ src/pages/Generic.js
@@ -12,7 +12,7 @@ const IndexPage = () => (
         <div className="inner">
           
           <img className="image-res" src={resume} alt="resume" />
-        
+         
         </div>
       </section>
     </article>
